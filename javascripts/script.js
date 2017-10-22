ec62364ea0c4d0b1b9b750bb1cc4b2bdb0431c55
Billy Tseng <tseng.billy@gmail.com>
2017-10-22 15:19:15 -0700

    feat: highlight the clicked marker.

--- javascripts/script.js
+++ javascripts/script.js
@@ -4,7 +4,8 @@ function initMap() {
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 32.2217429, lng: -110.926479},
-    zoom: 13
+    zoom: 13,
+    mapTypeControl: false
   });
 
   var locations = [
@@ -18,6 +19,10 @@ function initMap() {
 
   var largeInfowindow = new google.maps.InfoWindow();
   var bounds = new google.maps.LatLngBounds();
+
+  var defaultIcon = makeMarkerIcon('FE7569');
+  var clickedIcon = makeMarkerIcon('06E86C');
+
   // Initialize markers.
   for (var i = 0; i < locations.length; i++) {
     var position = locations[i].location;
@@ -28,29 +33,44 @@ function initMap() {
       position: position,
       title: title,
       animation: google.maps.Animation.DROP,
+      icon: defaultIcon,
       id: i
     });
     // Push the marker to array of markers.
     markers.push(marker);
     // Create an onclick event to open an infowindow at each marker.
     marker.addListener('click', function() {
+      // Turn all markers color to default color, and change the current marker
+      for (var j = 0; j < locations.length; j++) {
+        markers[j].setIcon(defaultIcon);
+      }
+      this.setIcon(clickedIcon);
+
       populateInfoWindow(this, largeInfowindow);
     });
     bounds.extend(markers[i].position);
   }
-    // Extend the boundaries of the map for each marker
-    map.fitBounds(bounds);
+  // Extend the boundaries of the map for each marker
+  map.fitBounds(bounds);
 }
 // This function populates the infowindow when the marker is clicked.
 function populateInfoWindow(marker, infowindow) {
-  // Check to make sure the infowindow is not already opened on this marker.
-  if (infowindow.marker != marker) {
-    infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div>');
-    infowindow.open(map, marker);
-    // Make sure the marker property is cleared if the infowindow is closed.
-    infowindow.addListener('closeclick',function(){
-      infowindow.setMarker = null;
-    });
-  }
+  infowindow.marker = marker;
+  infowindow.setContent('<div>' + marker.title + '</div>');
+  infowindow.open(map, marker);
+  // Make sure the marker property is cleared if the infowindow is closed.
+  infowindow.addListener('closeclick',function(){
+    infowindow.close();
+  });
+}
+
+function makeMarkerIcon(markerColor) {
+  var markerImage = new google.maps.MarkerImage(
+    'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+    '|40|_|%E2%80%A2',
+    new google.maps.Size(21, 34),
+    new google.maps.Point(0, 0),
+    new google.maps.Point(10, 34),
+    new google.maps.Size(21,34));
+  return markerImage;
 }

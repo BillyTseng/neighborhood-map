7ec4c3d8021391acb561c82d9a61dad5307269e4
Billy Tseng <tseng.billy@gmail.com>
2017-10-24 23:44:34 -0700

    style: add missing semi colon.

--- javascripts/script.js
+++ javascripts/script.js
@@ -3,7 +3,7 @@ var markers = [];
 var defaultIcon;
 var clickedIcon;
 function initMap() {
-  var centerOfMap = {lat: 32.2217429, lng: -110.926479}
+  var centerOfMap = {lat: 32.2217429, lng: -110.926479};
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: centerOfMap,
